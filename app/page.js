53ff1bff622604f0fd443ce3d02032937b4ad758
chandrasekar Mohankumar <chandru_noni@yahoo.com>
2025-06-17 02:04:13 +0530

    Fix import paths

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import { Card, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Input } from "../components/ui/input"
+import { Label } from "../components/ui/label"
 import { useState } from "react"
 
 export default function SafeDeals() {
