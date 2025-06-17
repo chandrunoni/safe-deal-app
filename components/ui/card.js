d5bd965e08355b611fc31d7bc3a3bd3d2253b860
chandrasekar Mohankumar <chandru_noni@yahoo.com>
2025-06-17 17:59:34 +0530

    Fix: CardContent not defined issue

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,7 +1,3 @@
 export function Card(props) {
   return <div className="card">{props.children}</div>;
 }
-
-export function CardContent(props) {
-  return <div className="card-content">{props.children}</div>;
-}
